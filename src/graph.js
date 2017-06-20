ec198a18e7c87e15ffed62ab8ec85019813bb6bc
Alexey Velikiy <gmpota@gmail.com>
2017-06-20 18:01:50 +0300

    vis extranal lib

---
+++ src/graph.js
@@ -0,0 +1,11 @@
+import * as vis from './extarnal/vis.min';
+
+
+export class Graph {
+  constructor($holder) {
+
+  }
+}
+
+
+import './extarnal/vis.min.css!';
--- src/module.js
+++ src/module.js
@@ -1,24 +1,20 @@
+import { Graph } from './graph';
+
 import { PanelCtrl } from 'app/plugins/sdk';
 import config from 'app/core/config';
+
 
 const MIN_HIGHT = 40;
 const TITLE_HIGHT = 25;
 
+
 export class Ctrl extends PanelCtrl {
   constructor($scope, $injector) {
     super($scope, $injector);
-    this.setDefaults();
     $scope.dashboard = this.dashboard;
 
     this.events.on('render', this.onRender.bind(this));
     this.events.on('panel-initialized', this.onRender.bind(this));
-  }
-
-  setDefaults() {
-    this.panel.title = '!!!';
-    if(this.row.isNew && this.row.panels.length === 1) {
-      this.row.height = MIN_HIGHT;
-    }
   }
 
   onRender() {
